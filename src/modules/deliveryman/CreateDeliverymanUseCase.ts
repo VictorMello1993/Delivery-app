091f61e611f5b1d8ab71b3734a28ccf0fa6907f1
Victor Mello <victor.mello@al.infnet.edu.br>
2022-05-10 17:32:36 -0300

    Cadastro e autenticação de entregador

---
+++ src/modules/deliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,32 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../database/prismaClient";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({username, password} : ICreateDeliveryman) {
+    const deliveryman = await prisma.deliveryman.findFirst({where: {
+      username: {
+        mode: "insensitive" //Ignorando os valores em caixa alta ou caixa baixa
+      }
+    } })
+
+    if(deliveryman){
+      throw new Error("Deliveryman already exists!")
+    }
+
+    const hashedPassword = await hash(password, 10)
+
+    const newDeliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashedPassword
+      }
+    })
+
+    return newDeliveryman
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import {Router} from 'express';
 import { AuthenticateClientController } from './modules/accounts/authClient/AuthenticateClientController';
+import { AuthenticateDeliverymanController } from './modules/accounts/authDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliverymanController } from './modules/deliveryman/CreateDeliverymanController';
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
+const createDeliverymanController = new CreateDeliverymanController()
+const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 
-routes.post('/login', authenticateClientController.handle)
+routes.post('/clients/login', authenticateClientController.handle)
+routes.post('/deliverymen/login', authenticateDeliverymanController.handle)
 routes.post('/clients', createClientController.handle)
+routes.post('/deliverymen', createDeliverymanController.handle)
 
 export {routes}
