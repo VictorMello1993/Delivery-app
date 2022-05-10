01bbe46e2f6a51ccd9e7c7dcb3161e19160ce51b
Victor Mello <victor.mello@al.infnet.edu.br>
2022-05-10 18:06:33 -0300

    Cadastro de entrega

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,9 @@ import {Router} from 'express';
 import { AuthenticateClientController } from './modules/accounts/authClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/accounts/authDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
-import { CreateDeliverymanController } from './modules/deliveryman/CreateDeliverymanController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/CreateDeliveryController';
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/CreateDeliverymanController';
+
 
 const routes = Router()
 
@@ -10,10 +12,12 @@ const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post('/clients/login', authenticateClientController.handle)
 routes.post('/deliverymen/login', authenticateDeliverymanController.handle)
 routes.post('/clients', createClientController.handle)
 routes.post('/deliverymen', createDeliverymanController.handle)
+routes.post('/deliveries',  createDeliveryController.handle)
 
 export {routes}
